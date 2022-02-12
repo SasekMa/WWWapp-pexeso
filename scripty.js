e366a6388f9711ad909dc5a64bb6c5e324998974
Martin Šašek <m.sasek@seznam.cz>
2022-02-12 18:57:56 +0100

    Ukládání jména karty do log - příprava na porovnání

--- scripty.js
+++ scripty.js
@@ -1,5 +1,10 @@
 const karty = document.querySelectorAll(".pex-karta");
 
+let uzJeOtoceno = false;
+let prvniKarta, druhaKarta;
+
+
+
 function otocKartu() {
 //test funkčnosti
 //console.log("Jo tu jsom");
@@ -7,6 +12,20 @@ function otocKartu() {
 
 //změň třídu
   this.classList.toggle("flip");
+
+  if (!uzJeOtoceno) {
+    // první click
+    uzJeOtoceno = true;
+    prvniKarta = this;
+  } else {
+    // druhý clik
+    uzJeOtoceno = false;
+    druhaKarta = this;
+
+   //mam dvě karty a teď jenom porovnat - příprava
+   console.log(prvniKarta.dataset.jmeno);
+   console.log(druhaKarta.dataset.jmeno);
+  }
 }
 
-karty.forEach(karta => karta.addEventListener("click", otocKartu));
+karty.forEach(karta => karta.addEventListener("click", otocKartu)); 
